fa519ded613d4659bcff381cccc1719ae5af7cf3
Keikhan <whatsonjohn552@gmail.com>
2025-04-16 01:40:46 +0500

    update 0.1

--- cmd/routes.go
+++ cmd/routes.go
@@ -12,5 +12,6 @@ func setupRoutes(app *fiber.App) {
 	app.Post("/fact", handlers.CreateFact)
 	app.Get("/fact/:id", handlers.ShowFact)
 	app.Get("/fact/:id/edit", handlers.EditFact)
+	// Update fact
 	app.Patch("/fact/:id", handlers.UpdateFact)
 }
